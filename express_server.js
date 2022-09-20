35177edbb27c89facd081e4dd9c5341c79c0d483
Jodie Drover <jodiedrover@live.com>
2022-09-20 09:47:40 -0400

    Add feature/cookies branch

--- express_server.js
+++ express_server.js
@@ -64,6 +64,11 @@ app.post("/urls/:id/delete", (req, res) => {
   res.redirect("/urls");
 });
 
+app.post("/urls/:id/update", (req, res) => {
+  urlDatabase[req.params.id] = req.body.longURL;
+  res.redirect("/urls");
+})
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
